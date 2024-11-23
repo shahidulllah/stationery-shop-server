11258c5664671320e61a950cfa5f92bd1edaa1aa
shahidullah177 <mdshahidsumon177@gmail.com>
2024-11-23 21:19:54 +0600

    Project setup done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,30 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node, // Add Node.js globals
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
   {
-    ignores: ["node_modules", "dist"],
+    ignores: ['node_modules', 'dist'],
     rules: {
-      "no-unused-vars": "error",
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
     },
   },
 ];
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,14 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
-const app: Application = express()
-const port = 3000
+const app: Application = express();
 
 //parser
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-export default app
-
+export default app;
