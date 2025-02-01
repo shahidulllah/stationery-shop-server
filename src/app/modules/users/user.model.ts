93768178603cbf15b909e4d2bfd16768a0d30e5e
shahidulllah <mdshahidsumon177@gmail.com>
2025-02-01 22:15:54 +0600

    user model and interface

---
+++ src/app/modules/users/user.model.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from 'mongoose';
+import { IUser } from './user.interface';
+
+const UserSchema: Schema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model<IUser>('User', UserSchema);
