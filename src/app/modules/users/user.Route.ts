17bf34120606e93795934015cc0d1d776222c9b2
shahidulllah <mdshahidsumon177@gmail.com>
2025-02-06 02:22:07 +0600

    working on use route

--- src/app/modules/users/user.Route.ts
+++ src/app/modules/users/user.Route.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import { deleteUser, getAllUsers, updateUserRole } from "./user.controller";
+import express from 'express';
+import { deleteUser, getAllUsers, updateUserRole } from './user.controller';
 
 const router = express.Router();
 
-router.get("/", getAllUsers);
-router.put("/:userId/role", updateUserRole);
-router.delete("/:userId", deleteUser);
+router.get('/', getAllUsers);
+router.put('/:userId/role', updateUserRole);
+router.delete('/:userId', deleteUser);
 
 export default router;
