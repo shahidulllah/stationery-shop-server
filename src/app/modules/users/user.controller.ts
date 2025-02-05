2323d15f49656e45def1f584ebec85e328a3625f
shahidulllah <mdshahidsumon177@gmail.com>
2025-02-06 01:33:37 +0600

    working on manage user

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { loginUser, registerUser } from './user.service';
+import User from './user.model';
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -17,5 +18,57 @@ export const login = async (req: Request, res: Response) => {
     res.json(result);
   } catch (error: any) {
     res.status(400).json({ message: error.message });
+  }
+};
+
+// Get all users
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find().select('-password'); // Exclude password from response
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching users' });
+  }
+};
+
+// Update user role
+export const updateUserRole = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const { role } = req.body;
+
+  if (!['user', 'admin'].includes(role)) {
+    return res.status(400).json({ message: 'Invalid role' });
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { role },
+      { new: true },
+    ).select('-password');
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating role' });
+  }
+};
+
+// Delete user
+export const deleteUser = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting user' });
   }
 };
